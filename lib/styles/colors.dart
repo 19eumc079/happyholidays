0aa4a374856ab10c413d9afb130dafc36281b442
Kumaran <kumaransk1608@gmail.com>
2023-01-21 15:34:31 +0530

    add happyholidays

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,10 +1,7 @@
 part of styles;
 
 class AppColor {
-  static const Color primaryColor = Color.fromRGBO(
-    8,
-    8,
-    8,
-    8,
-  );
+  static const Color primaryColor = Color.fromARGB(248, 250, 249, 249);
+  static const Color secondaryColor = Color.fromARGB(1, 0, 0, 0);
+  static const Color teritaryColor = Color.fromARGB(90, 248, 245, 245);
 }
